c003175e49be22d4ec4e427712bb751ad0abac01
andresfleon <andresfleon@gmail.com>
2024-01-16 00:37:27 -0500

    v0.0 a

--- src/components/ConfirmationCard.jsx
+++ src/components/ConfirmationCard.jsx
@@ -10,7 +10,7 @@ const ConfirmationCard = ({setIsConfirmation,userDelete}) => {
     <article className='window__msg' >
         <div className='form__x' onClick={handleExit}>x</div>
         <h2 className='title__confirmation'>Eliminar Usuario</h2>
-        <p className='msg__confirmation'>El Usuario {userDelete}ha sido eliminado</p>
+        <p className='msg__confirmation'>El Usuario <span className='user__delete'>{userDelete}</span> ha sido eliminado</p>
         <button className='btn__confirmation' onClick={handleExit}>Aceptar</button>
     </article>
   )
